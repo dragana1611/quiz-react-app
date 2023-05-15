f771d21631ef4ed476b1466116742273d7f9475f
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-05-15 21:59:51 +0200

    completed project

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     correct,
     nextQuestion,
     checkAnswer,
+    closeModal,
   } = useGlobalContext();
 
   if (waiting) {
@@ -23,6 +24,17 @@ function App() {
     return <Loading />;
   }
 
+  const { question, incorrect_answers, correct_answer } = questions[index];
+  // const answers = [...incorrect_answers, correct_answer];
+  let answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
+  if (tempIndex === 3) {
+    answers.push(correct_answer);
+  } else {
+    answers.push(answers[tempIndex]);
+    answers[tempIndex] = correct_answer;
+  }
+
   return (
     <main>
       <Modal />
@@ -31,10 +43,10 @@ function App() {
           correct answers : {correct}/{index}
         </p>
         <article className="container">
-          {/* <h2 dangerouslySetInnerHTML={{ __html: question }} /> */}
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
           <div className="btn-container">
-            {/* {answers.map((answer, index) => { */}
-            {/* return (
+            {answers.map((answer, index) => {
+              return (
                 <button
                   key={index}
                   className="answer-btn"
@@ -42,7 +54,7 @@ function App() {
                   dangerouslySetInnerHTML={{ __html: answer }}
                 />
               );
-            })} */}
+            })}
           </div>
         </article>
         <button className="next-question" onClick={nextQuestion}>
@@ -54,3 +66,5 @@ function App() {
 }
 
 export default App;
+
+//dangerouslySetInnerHTML is a property that you can use on HTML elements in a React application to programmatically set their content. Instead of using a selector to grab the HTML element, then setting its innerHTML, you can use this property directly on the element. React skips the comparison against the virtual DOM to gain some extra performance. The output would be just the string.
--- src/context.js
+++ src/context.js
@@ -1,33 +1,99 @@
 import axios from "axios";
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 
 const table = {
+  general_knowledge: 9,
+  film: 11,
   sports: 21,
+  georaphy: 22,
   history: 23,
   politics: 24,
+  animals: 27,
 };
 
 const API_ENDPOINT = "https://opentdb.com/api.php?";
 
-const url = "";
-const tempUrl =
-  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+// const tempUrl =
+//   "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [waiting, setWaiting] = useState(true);
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [index, setIndex] = useState(0);
   const [correct, setCorrect] = useState(0);
   const [error, setError] = useState(false);
   const [quiz, setQuiz] = useState({
     amount: 10,
-    category: "sports",
+    category: "general_knowledge",
     difficulty: "easy",
   });
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+    const response = await axios(url).catch((err) => console.log(err));
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+
+  const nextQuestion = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex + 1;
+      if (index > questions.length - 1) {
+        openModal();
+        return 0;
+      } else {
+        return index;
+      }
+    });
+  };
+
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((oldState) => oldState + 1);
+    }
+    nextQuestion();
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setWaiting(true);
+    setCorrect(0);
+    setIsModalOpen(false);
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setQuiz({ ...quiz, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { amount, category, difficulty } = quiz;
+
+    const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`;
+    fetchQuestions(url);
+  };
 
   return (
     <AppContext.Provider
@@ -39,6 +105,12 @@ const AppProvider = ({ children }) => {
         correct,
         error,
         isModalOpen,
+        nextQuestion,
+        checkAnswer,
+        closeModal,
+        quiz,
+        handleChange,
+        handleSubmit,
       }}
     >
       {children}
